ba21dedd3e77b368acbc8683533471e36f324502
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-08 04:21:03 +0430

    Add BaseEntity

---
+++ ToDo/src/main/java/ir/maktab56/ToDo/base/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package ir.maktab56.ToDo.base.domain;
+
+public class BaseEntity<ID> {
+
+    private ID id;
+
+    private Boolean isDeleted;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+}
