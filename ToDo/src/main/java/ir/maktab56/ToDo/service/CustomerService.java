0c944ceaa327f62743ed429a3d95260cdc94f91d
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-08 23:25:34 +0430

    Add CustomerService

---
+++ ToDo/src/main/java/ir/maktab56/ToDo/service/CustomerService.java
@@ -0,0 +1,8 @@
+package ir.maktab56.ToDo.service;
+
+import ir.maktab56.ToDo.base.service.BaseService;
+import ir.maktab56.ToDo.domain.Customer;
+
+public interface CustomerService extends BaseService<Customer, Long> {
+
+}
