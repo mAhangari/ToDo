15ea6baa85c9a4383220d761047e61721a31c1e7
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-09 03:58:41 +0430

    Update AdminRepositoryImpl

--- ToDo/src/main/java/ir/maktab56/ToDo/repository/impl/AdminRepositoryImpl.java
+++ ToDo/src/main/java/ir/maktab56/ToDo/repository/impl/AdminRepositoryImpl.java
@@ -1,6 +1,5 @@
 package ir.maktab56.ToDo.repository.impl;
 
-import java.sql.SQLException;
 import java.util.*;
 import javax.persistence.*;
 import ir.maktab56.ToDo.base.reposiotry.impl.BaseRepositoryImpl;
@@ -12,36 +11,56 @@ public class AdminRepositoryImpl extends BaseRepositoryImpl<Admin, Long> impleme
 	public AdminRepositoryImpl(EntityManagerFactory emf) {
 		super(emf);
 	}
-
-	public void save(Admin admin) throws SQLException {
+	
+	@Override
+	public void save(Admin admin) {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 			em.persist(admin);
 		em.getTransaction().commit();
 		em.close();
 	}
-
-	public void update(Admin admin) throws SQLException {
+	
+	@Override
+	public void update(Admin admin) {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 			em.merge(admin);
 		em.getTransaction().commit();
 		em.close();
 	}
-
+	
+	@Override
 	public List<Admin> findAllById(Collection<Long> ids) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager en = emf.createEntityManager();
+		try {
+			List<Admin> admins = new ArrayList<>();
+			for(Long id: ids) {
+				admins.add(findById(id));
+			}
+			return admins;
+		}catch(NoResultException e) {
+			return null;
+		}finally {
+			en.close();
+		}
 	}
-
-	public List<Admin> findAll() throws SQLException {
+	
+	@Override
+	public List<Admin> findAll() {
 		EntityManager em = emf.createEntityManager();
-		List<Admin> admins = em.createQuery("SELECT * FROM admin", Admin.class).getResultList();
-		em.close();
-		return admins;
+		try{
+			List<Admin> admins = em.createQuery("SELECT * FROM Admin", Admin.class).getResultList();
+			return admins;
+		}catch(NoResultException e) {
+			return null;
+		}finally {
+			em.close();
+		}
 	}
-
-	public void deleteById(Long id) throws SQLException {
+	
+	@Override
+	public void deleteById(Long id) {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 			Admin admin = em.find(Admin.class, id);
@@ -49,15 +68,17 @@ public class AdminRepositoryImpl extends BaseRepositoryImpl<Admin, Long> impleme
 		em.getTransaction().commit();
 		em.close();
 	}
-
-	public Admin findById(Long id) throws SQLException {
+	
+	@Override
+	public Admin findById(Long id) {
 		EntityManager em = emf.createEntityManager();
 		Admin admin = em.find(Admin.class, id);
 		em.close();
 		return admin;
 	}
-
-	public Boolean existsById(Long id) throws SQLException {
+	
+	@Override
+	public Boolean existsById(Long id) {
 		EntityManager em = emf.createEntityManager();
 		if(em.find(Admin.class, id) != null) {
 			em.close();
@@ -65,5 +86,48 @@ public class AdminRepositoryImpl extends BaseRepositoryImpl<Admin, Long> impleme
 		}
 		em.close();
 		return false;
+	}
+	
+	@Override
+	public <T> boolean checkUsername(T username) {
+		EntityManager em = emf.createEntityManager();
+		try {
+			Query query = em.createQuery("SELECT * FROM Admin AS a WHERE a.username =: Username", Admin.class);
+			query.setParameter("Username", username).getSingleResult();
+			return true;
+		}catch(NoResultException e) {
+			return false;
+		}finally {
+			em.close();
+		}
+	}
+	
+	@Override
+	public <UT, PT> boolean checkAdminInfo(UT username, PT password) {
+		EntityManager em = emf.createEntityManager();
+		try {
+			Query query = em.createQuery("SELECT * FROM Admin AS a WHERE a.username =: Username AND c.password =: Password", Admin.class); 
+			query.setParameter("Username", username);
+			query.setParameter("Password", password);
+			query.getSingleResult();
+			return true;
+		}catch(NoResultException e) {
+			return false;
+		}finally{
+			em.close();
+		}
+	}
+	
+	@Override
+	public <T> Admin findByUsername(T username) {
+		EntityManager em = emf.createEntityManager();
+		try {
+			Admin admin = em.createQuery("SELECT c FROM Admin AS c WHERE c.username =: Username", Admin.class).setParameter("Username", username).getSingleResult();
+			return admin;
+		}catch(NoResultException e) {
+			return null;
+		}finally {
+			em.close();
+		}
 	}
 }
