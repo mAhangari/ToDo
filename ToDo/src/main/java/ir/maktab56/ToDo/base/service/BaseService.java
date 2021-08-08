bf48e79053d100cdd3c49f0d8c999e60d8521ec2
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-08 04:44:55 +0430

    Add BaseService

---
+++ ToDo/src/main/java/ir/maktab56/ToDo/base/service/BaseService.java
@@ -0,0 +1,23 @@
+package ir.maktab56.ToDo.base.service;
+
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+import ir.maktab56.ToDo.base.domain.BaseEntity;
+
+public interface BaseService<E extends BaseEntity<ID>, ID> {
+	
+	void save(E e) throws SQLException;
+
+    void update(E e) throws SQLException;
+
+    List<E> findAllById(Collection<ID> ids);
+
+    List<E> findAll() throws SQLException;
+
+    void deleteById(ID id) throws SQLException;
+
+    E findById(ID id) throws SQLException;
+
+    Boolean existsById(ID id) throws SQLException;
+}
