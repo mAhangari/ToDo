535248f8792e824d78efa73ea5bb413a84739033
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-08 15:52:44 +0430

    Add Admin

---
+++ ToDo/src/main/java/ir/maktab56/ToDo/domain/Admin.java
@@ -0,0 +1,25 @@
+package ir.maktab56.ToDo.domain;
+
+import javax.persistence.Column;
+
+public class Admin extends User{
+	
+	@Column(name = "is_super_admin", columnDefinition = "TINYINT(1)")
+	private Boolean isSuperAdmin;
+	
+	public Admin(Long id, Boolean isDeleted, String username, String password, String nationalCode, String firstName,
+			String lastName, String email, String mobileNumber, String userType, Boolean isActive,
+			Boolean isSuperAdmin) {
+		super(id, isDeleted, username, password, nationalCode, firstName, lastName, email, mobileNumber, userType,
+				isActive);
+		this.setIsSuperAdmin(isSuperAdmin);
+	}
+
+	public Boolean getIsSuperAdmin() {
+		return isSuperAdmin;
+	}
+
+	public void setIsSuperAdmin(Boolean isSuperAdmin) {
+		this.isSuperAdmin = isSuperAdmin;
+	}
+}
