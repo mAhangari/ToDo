87cc54083fef74be46e7fb7e512277f608a58095
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-08 16:52:39 +0430

    Refactor BaseRepsitoryImpl

--- ToDo/src/main/java/ir/maktab56/ToDo/base/reposiotry/impl/BaseRepositoryImpl.java
+++ ToDo/src/main/java/ir/maktab56/ToDo/base/reposiotry/impl/BaseRepositoryImpl.java
@@ -5,7 +5,7 @@ import ir.maktab56.ToDo.base.domain.BaseEntity;
 import ir.maktab56.ToDo.base.reposiotry.BaseRepository;
 
 public abstract class BaseRepositoryImpl<E extends BaseEntity<ID>, ID>
-			implements BaseRepository<E, ID>{
+			implements BaseRepository<E, ID> {
 	protected final Connection connection;
 	
 	public BaseRepositoryImpl(Connection connection) {
