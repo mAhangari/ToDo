cdd9150f9cfd315f55a1671f3f91e957804b334f
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-09 04:00:01 +0430

    Update App

--- ToDo/src/main/java/ir/maktab56/ToDo/App.java
+++ ToDo/src/main/java/ir/maktab56/ToDo/App.java
@@ -1,15 +1,16 @@
 package ir.maktab56.ToDo;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+// import java.sql.Date;
+import java.sql.SQLException;
+// import ir.maktab56.ToDo.domain.Customer;
 import ir.maktab56.ToDo.util.*;
 
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	EntityManagerFactory emf = HibernateUtil.getEntityMangerFactory();
-    	EntityManager entityManager = emf.createEntityManager();
+public class App {
+    public static void main( String[] args ) throws SQLException {
+    	ApplicationContext.getConnection();
+    	ApplicationContext.menu.showMenu();
+    	//Customer customer1 = new Customer(1l, false, "mAhangari", "Mori1995", "2150382342", "Morteza", "Ahangari", "morteza_1993@yahoo.com", "09116425909", "Customer", true, Date.valueOf("1995-02-16"));
+    	ApplicationContext.emf.close();
     }
 }
