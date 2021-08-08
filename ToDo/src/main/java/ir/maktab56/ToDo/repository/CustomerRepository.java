22361c64eaceafe58bbd2c05b03d34562bb5b4ba
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-08 16:51:18 +0430

    Add CustomerRepository

---
+++ ToDo/src/main/java/ir/maktab56/ToDo/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package ir.maktab56.ToDo.repository;
+
+import ir.maktab56.ToDo.base.reposiotry.BaseRepository;
+import ir.maktab56.ToDo.domain.Customer;
+
+public interface CustomerRepository extends BaseRepository<Customer, Long> {
+
+}
