5ca17f1c8b6ee9797fc295c01f1c85d84bf11fca
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-08 23:25:15 +0430

    Add AdminService

---
+++ ToDo/src/main/java/ir/maktab56/ToDo/service/AdminService.java
@@ -0,0 +1,8 @@
+package ir.maktab56.ToDo.service;
+
+import ir.maktab56.ToDo.base.service.BaseService;
+import ir.maktab56.ToDo.domain.Admin;
+
+public interface AdminService extends BaseService<Admin, Long> {
+
+}
