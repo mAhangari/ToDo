0044fa3ad7d847416b0a399c1fc626de5c2eb848
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-09 20:25:02 +0430

    Add WalletRepository

---
+++ ToDo/src/main/java/ir/maktab56/ToDo/repository/WalletRepository.java
@@ -0,0 +1,8 @@
+package ir.maktab56.ToDo.repository;
+
+import ir.maktab56.ToDo.base.reposiotry.BaseRepository;
+import ir.maktab56.ToDo.domain.Wallet;
+
+public interface WalletRepository extends BaseRepository<Wallet, Long> {
+
+}
