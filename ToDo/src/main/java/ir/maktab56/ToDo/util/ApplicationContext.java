032f504b48ee48bbc35971c2e958df7728f74735
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-09 21:26:12 +0430

    Update ApplicationContext

--- ToDo/src/main/java/ir/maktab56/ToDo/util/ApplicationContext.java
+++ ToDo/src/main/java/ir/maktab56/ToDo/util/ApplicationContext.java
@@ -1,19 +1,24 @@
 package ir.maktab56.ToDo.util;
 
 import javax.persistence.EntityManagerFactory;
-import ir.maktab56.ToDo.repository.impl.AdminRepositoryImpl;
-import ir.maktab56.ToDo.repository.impl.CustomerRepositoryImpl;
-import ir.maktab56.ToDo.service.impl.AdminServiceImpl;
-import ir.maktab56.ToDo.service.impl.CustomerServiceImpl;
-import ir.maktab56.ToDo.service.impl.Menu;
+import ir.maktab56.ToDo.repository.impl.*;
+import ir.maktab56.ToDo.service.impl.*;
 
 public class ApplicationContext {
 	
 	public static EntityManagerFactory emf = HibernateUtil.getEntityMangerFactory();
+	
 	public static CustomerRepositoryImpl customerRepo = new CustomerRepositoryImpl(emf);
 	public static CustomerServiceImpl customerServ = new CustomerServiceImpl(customerRepo);
+	
 	public static AdminRepositoryImpl adminRepo = new AdminRepositoryImpl(emf);
 	public static AdminServiceImpl adminServ = new AdminServiceImpl(adminRepo);
+	
+	public static WalletRepositoryImpl walletRepo = new WalletRepositoryImpl(emf);
+	public static WalletServiceImpl walletServ = new WalletServiceImpl(walletRepo);
+	
+	public static ActivityRepositoryImpl activityRepo = new ActivityRepositoryImpl(emf);
+	public static ActivityServiceImpl activityServ = new ActivityServiceImpl(activityRepo);
 
 	public static Menu menu = new Menu();
 	public static CheckInputInformation chInInformation = new CheckInputInformation();	
