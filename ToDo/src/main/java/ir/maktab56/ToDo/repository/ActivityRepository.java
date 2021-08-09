b26b81a8fb66414159fc979d02fe085aa46834a6
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-09 21:18:29 +0430

    Update ActivityRepository

--- ToDo/src/main/java/ir/maktab56/ToDo/repository/ActivityRepository.java
+++ ToDo/src/main/java/ir/maktab56/ToDo/repository/ActivityRepository.java
@@ -1,8 +1,12 @@
 package ir.maktab56.ToDo.repository;
 
+import java.util.List;
+
 import ir.maktab56.ToDo.base.reposiotry.BaseRepository;
 import ir.maktab56.ToDo.domain.Activity;
 
 public interface ActivityRepository extends BaseRepository<Activity, Long>  {
+
+	<T> List<Activity> findByUserId(T id);
 
 }
