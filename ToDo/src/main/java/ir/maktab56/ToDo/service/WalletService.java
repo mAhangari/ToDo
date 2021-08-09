a1277c9b8c11166a335e615b72a570ef36c0a232
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-09 20:40:31 +0430

    Add WalletService

---
+++ ToDo/src/main/java/ir/maktab56/ToDo/service/WalletService.java
@@ -0,0 +1,8 @@
+package ir.maktab56.ToDo.service;
+
+import ir.maktab56.ToDo.base.service.BaseService;
+import ir.maktab56.ToDo.domain.Wallet;
+
+public interface WalletService extends BaseService<Wallet, Long> {
+
+}
