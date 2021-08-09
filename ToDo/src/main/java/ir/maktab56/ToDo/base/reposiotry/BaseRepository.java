7ce18ebd7990f3c829b600d4d4202f5b716cc2e5
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-09 21:20:33 +0430

    Update BaseRepository

--- ToDo/src/main/java/ir/maktab56/ToDo/base/reposiotry/BaseRepository.java
+++ ToDo/src/main/java/ir/maktab56/ToDo/base/reposiotry/BaseRepository.java
@@ -1,6 +1,5 @@
 package ir.maktab56.ToDo.base.reposiotry;
 
-import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,17 +7,18 @@ import ir.maktab56.ToDo.base.domain.*;
 
 public interface BaseRepository<E extends BaseEntity<ID>, ID> {
 
-    void save(E e) throws SQLException;
+    void save(E e);
 
-    void update(E e) throws SQLException;
+    void update(E e);
 
     List<E> findAllById(Collection<ID> ids);
 
-    List<E> findAll() throws SQLException;
+    List<E> findAll();
 
-    void deleteById(ID id) throws SQLException;
+    void deleteById(ID id);
 
-    E findById(ID id) throws SQLException;
+    E findById(ID id);
 
-    Boolean existsById(ID id) throws SQLException;
+    Boolean existsById(ID id);
+    
 }
