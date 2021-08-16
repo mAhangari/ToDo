3af22265552148bc5e2f473e0d1ddd7a3f384071
morteza <morteza_ahangari_1994@yahoo.com>
2021-08-16 18:02:41 +0430

    Publish App version 1.1

--- ToDo/src/main/java/ir/maktab56/ToDo/App.java
+++ ToDo/src/main/java/ir/maktab56/ToDo/App.java
@@ -3,7 +3,6 @@ package ir.maktab56.ToDo;
 import java.sql.SQLException;
 import ir.maktab56.ToDo.util.*;
 
-
 public class App {
     public static void main( String[] args ) throws SQLException {
     	ApplicationContext.menu.showMenu();
